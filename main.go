77dc51429ca3326b1b8decb22bdceaed30d22bbc
Felipe <ftoniolo.aat@gmail.com>
2020-05-04 06:29:10 -0300

    colocando no console os status

--- main.go
+++ main.go
@@ -3,14 +3,32 @@ package main
 import (
 	f "fmt"
 	"physicalsales/clients"
+	"strconv"
 )
 
 func main() {
 
+	//Gera o token
 	token := clients.GetToken().AccessToken
+	f.Println("O token gerado foi : ", token)
 
-	if token != "" {
-		f.Println("O token gerado foi : ", token)
-	}
+	//Gera o Pagamento
+	generatePayment := clients.CreatePayment(token)
+	f.Println("O Pagamento foi gerado e o paymentId é : " + generatePayment.Payment.PaymentId)
+
+	//Confirma o Pagamento
+	confirm := clients.PaymentConfirm(token, generatePayment.Payment.PaymentId)
+	confirmStatus := strconv.Itoa(confirm.Status)
+	f.Println("O Pagamento foi confirmado e o status é : " + confirmStatus)
+
+	//Cancela o Pagamento
+	cancelation := clients.CancelationConfirm(token, generatePayment.Payment.PaymentId)
+	cancelationStatus := strconv.Itoa(cancelation.Status)
+	f.Println("O Cancelamento do pagamento foi confirmado e o status é : " + cancelationStatus)
+
+	//Desfaz o Cancelamento
+	undoCancelation := clients.UndoCancelation(token, generatePayment.Payment.PaymentId, cancelation.VoidId)
+	undoCancelationStatus := strconv.Itoa(undoCancelation.Status)
+	f.Println("O Status do Desfazimento do Cancelamento de Pagamento é: " + undoCancelationStatus)
 
 }
